5d6fbab1aa83b307447a7451478e58e73c1697dc
Yousuf Shabibi <145971672+Shabibi2003@users.noreply.github.com>
2025-04-04 16:14:56 +0530

    Update test.py

--- test.py
+++ test.py
@@ -143,7 +143,7 @@ st.markdown("""
             font-size: 36px;
             text-align: center;
             padding: 20px;
-            border-radius: 10px;
+            border-radius: 20px;
             border-bottom: 4px solid red;  /* Red underline */
         }
         .red-line {
