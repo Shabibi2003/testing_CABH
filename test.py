c7b82e549a4970e3f1552b3c50ad6c91b6a1e82d
Yousuf Shabibi <145971672+Shabibi2003@users.noreply.github.com>
2025-04-11 16:19:15 +0530

    Update test.py

--- test.py
+++ test.py
@@ -455,7 +455,7 @@ if st.button("Generate Charts"):
                 st.markdown("<h3 style='font-size:30px; text-align:center; font-weight:bold;'>Indoor vs Outdoor Scatter Plots</h3>", unsafe_allow_html=True)
                 st.markdown("<br>", unsafe_allow_html=True)
                 # plot_indoor_vs_hour_scatter(indoor_df_month, ['aqi', 'pm10', 'pm25', 'co2', 'voc'], year, selected_month, all_figs)
-                pollutants = ["aqi", "pm10", "pm25", "co2", "voc"]
+                pollutants = ["aqi", "pm10", "pm25"]
                 # plot_indoor_vs_outdoor_scatter(indoor_df_month, outdoor_df, pollutants, all_figs)
                 plot_indoor_vs_outdoor_scatter(indoor_df_month, outdoor_df, pollutants, all_figs)
 
