ea3b9910c2a80405e5ef79d208165d569fc5833f
Yousuf Shabibi <145971672+Shabibi2003@users.noreply.github.com>
2025-04-15 12:47:11 +0530

    Update test.py

--- test.py
+++ test.py
@@ -407,7 +407,7 @@ if st.button("Generate Charts"):
                 st.markdown("<br>", unsafe_allow_html=True)
                 st.markdown("<h3 style='font-size:30px; text-align:center; font-weight:bold;'>Indoor vs Outdoor Scatter Plots</h3>", unsafe_allow_html=True)
                 st.markdown("<br>", unsafe_allow_html=True)
-                plot_indoor_vs_outdoor_scatter(indoor_df_hourly, outdoor_df_hourly, ['aqi', 'pm10', 'pm25'], all_figs)
+                plot_indoor_vs_outdoor_scatter(indoor_df_hourly, outdoor_df_hourly, pollutant_display_names, all_figs)
 
 
             else:
