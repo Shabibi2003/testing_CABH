016d27e7f20312ed055329f47cfe8fe4732dac40
Yousuf Shabibi <145971672+Shabibi2003@users.noreply.github.com>
2025-04-24 11:12:12 +0530

    Update test.py

--- test.py
+++ test.py
@@ -620,7 +620,7 @@ if st.button("Generate Charts"):
                     plot_indoor_vs_outdoor_scatter(indoor_df_hourly, outdoor_df_hourly, ['aqi', 'pm10', 'pm25'], all_figs)
 
                     st.markdown("<br>", unsafe_allow_html=True)
-                    st.markdown("<h3 style='font-size:30px; text-align:left; font-weight:bold;'>Indoor vs Outdoor Scatter Plots</h3>", unsafe_allow_html=True)
+                    st.markdown("<h3 style='font-size:30px; text-align:left; font-weight:bold;'>Hourly Average Heat Index</h3>", unsafe_allow_html=True)
                     st.markdown("<br>", unsafe_allow_html=True)
                     plot_hourly_heat_index_chart(indoor_df_hourly, all_figs)
 
