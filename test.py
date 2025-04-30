03bbeb5f16599c77397a5217b0aaa97b7bc21899
Yousuf Shabibi <shabibiyousuf@gmail.com>
2025-04-30 12:58:40 +0530

    time interval

--- test.py
+++ test.py
@@ -331,13 +331,13 @@ def plot_indoor_vs_outdoor_scatter(indoor_df, outdoor_df, pollutants, all_figs):
 
         # Define time intervals for classification
         time_buckets = {
-            '(4 AM - 7 AM)': (4,7),
+            '(12 AM - 3 AM)': (24,3),
             "Breakfast (7 AM - 9 AM)": (7, 8),
             "Lunch (1 PM - 2 PM)": (13, 14),
             '(3 PM - 7 PM)':(15, 19),
             "Dinner (7 PM - 10 PM)": (19, 21),
             '(10 PM - 12 AM)':(22,24),
-            '(12 AM - 3 AM)':(24,3)
+            '(12 AM - 3 AM)':(3,7)
         }
 
         interval_labels = []
